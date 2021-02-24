43f0e848eb9caf546bb6ff86cbb562fa3f3bb5cc
Andrew Harcourt <andrew.harcourt@octopus.com>
2021-02-24 12:08:07 +1000

    Add a very rudimentary command-line switch to allow us to display our own version number. This will help us when installing the tool as we can launch it to confirm that it's installed correctly.

--- source/OctoVersion.Tool/Program.cs
+++ source/OctoVersion.Tool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using OctoVersion.Core;
 using OctoVersion.Core.Configuration;
 
@@ -8,10 +9,18 @@ namespace OctoVersion.Tool
     {
         static void Main(string[] args)
         {
-            var (appSettings, configuration) = ConfigurationBootstrapper.Bootstrap<AppSettings>(args);
-
-            var runner = new OctoVersionRunner(appSettings, configuration);
-            runner.Run(out _);
+            // Very hacky args parsing. The intention is to revisit this soon. Please don't copy :)
+            if (args.Any(a => string.Equals(a, "--version", StringComparison.OrdinalIgnoreCase)))
+            {
+                var ourOwnVersion = typeof(Program).Assembly.GetName().Version;
+                Console.WriteLine(ourOwnVersion);
+            }
+            else
+            {
+                var (appSettings, configuration) = ConfigurationBootstrapper.Bootstrap<AppSettings>(args);
+                var runner = new OctoVersionRunner(appSettings, configuration);
+                runner.Run(out _);
+            }
         }
     }
 }
